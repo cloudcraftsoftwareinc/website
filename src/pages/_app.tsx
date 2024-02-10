7a05256361a66921d4f9646a13918a849ae53c22
Aaron Fay <aaron.j.fay@gmail.com>
2024-02-09 17:10:13 -0800

    move gtm script out of Head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,13 @@ import '../styles/global.css';
 
 import { MDXProvider } from '@mdx-js/react';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 import { GetStartedBlurb } from '../components/get-started-blurb';
 import * as mdxComponents from '../components/mdx';
 import { ServicesBlurb } from '../components/services-blurb';
+
+const gtmId = 'G-NV5LK0T9C1';
 
 const components = {
   ...mdxComponents,
@@ -15,6 +18,16 @@ const components = {
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <MDXProvider components={components}>
+    <Script src={`https://www.googletagmanager.com/gtag/js?id=${gtmId}`} />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){window.dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${gtmId}');
+      `}
+    </Script>
     <Component {...pageProps} />
   </MDXProvider>
 );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,6 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
-import Script from 'next/script';
+import Document, { Html, Main, NextScript } from 'next/document';
 
 import { AppConfig } from '../utils/AppConfig';
-
-const gtmId = 'G-NV5LK0T9C1';
 
 // Need to create a custom _document because i18n support is not compatible with `next export`.
 class MyDocument extends Document {
@@ -11,20 +8,6 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang={AppConfig.locale}>
-        <Head>
-          <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${gtmId}`}
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){window.dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', '${gtmId}');
-            `}
-          </Script>
-        </Head>
         <body className="testing">
           <Main />
           <NextScript />
